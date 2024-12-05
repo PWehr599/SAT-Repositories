1e4394c3f622b021814f67e36fb248dd2a5804f4
Philippe <P.Wehr@campus.lmu.de>
2024-12-05 16:55:02 +0100

    plotting

--- satqubolib_groundstates/ground_states_tabu/plot.py
+++ satqubolib_groundstates/ground_states_tabu/plot.py
@@ -1,19 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 def plot_data(df, save_dir):
-    #
-    df['Ground_State_Count'] = df['Ground_State_Count'].astype(float)
-    df_sorted = df.sort_values(by='Ground_State_Count')
-    attributes = ['Si_num_min_clauses_satisfied', 'Si_num_max_clauses_satisfied', 'Si_avg_clauses_satisified']
 
-    for attribute in attributes:
-        ax = df_sorted.plot(x='Ground_State_Count', y=attribute, kind='line', marker='o')
+    df['Ground_State_Count'] = df['Ground_State_Count'].astype(float)
+
+    df_sorted = df.sort_values(by='Ground_State_Count')
+
+    attributes = {
+        'Si_num_min_clauses_satisfied': 'Number of minimal Clauses satisfied',
+        'Si_num_max_clauses_satisfied': 'Number of maximal Clauses satisfied',
+        'Si_avg_clauses_satisified': 'Average Clauses satisfied'
+    }
+
+    for attribute, label in attributes.items():
+        ax = df_sorted.plot(x='Ground_State_Count', y=attribute, kind='scatter', marker='x')
 
         ax.set_xscale('log')
-        plt.title(f"{attribute} vs Ground_State_Count")
-        plt.xlabel("Ground_State_Count")
-        plt.ylabel(attribute)
+
+        plt.title(f"{label} vs Ground State Count")
+        plt.xlabel("Ground State Count")
+        plt.ylabel(label)
+
         plt.grid()
+
         plt.savefig(f"{save_dir}/{attribute}.png")
+
+        plt.clf()
